64c029b2af869f25041db28fa29d2f5fcc3c0952
Jonggu Lee <jg.jake.lee@gmail.com>
2022-05-22 17:45:21 +0900

    Add NAT gateway and unset default public ip

--- vpc.tf
+++ vpc.tf
@@ -14,7 +14,6 @@ resource "aws_subnet" "sec-gwlb-2a" {
   vpc_id     = aws_vpc.sec-vpc.id
   cidr_block = "10.20.1.0/24"
   availability_zone = "ap-northeast-2a"
-  map_public_ip_on_launch	= true
 
   tags = {
     Name = "sec-gwlb-2a"
@@ -25,7 +24,7 @@ resource "aws_subnet" "sec-gwlb-2c" {
   vpc_id     = aws_vpc.sec-vpc.id
   cidr_block = "10.20.3.0/24"
   availability_zone = "ap-northeast-2c"
-  map_public_ip_on_launch	= true
+
 
   tags = {
     Name = "sec-gwlb-2c"
@@ -36,7 +35,6 @@ resource "aws_subnet" "sec-pub-2a" {
   vpc_id     = aws_vpc.sec-vpc.id
   cidr_block = "10.20.11.0/24"
   availability_zone = "ap-northeast-2a"
-  map_public_ip_on_launch	= true
 
   tags = {
     Name = "sec-pub-2a"
@@ -47,7 +45,6 @@ resource "aws_subnet" "sec-pub-2c" {
   vpc_id     = aws_vpc.sec-vpc.id
   cidr_block = "10.20.13.0/24"
   availability_zone = "ap-northeast-2c"
-  map_public_ip_on_launch	= true
 
   tags = {
     Name = "sec-pub-2c"
@@ -58,7 +55,6 @@ resource "aws_subnet" "sec-tgw-2a" {
   vpc_id     = aws_vpc.sec-vpc.id
   cidr_block = "10.20.251.0/24"
   availability_zone = "ap-northeast-2a"
-  map_public_ip_on_launch	= true
 
   tags = {
     Name = "sec-tgw-2a"
@@ -69,7 +65,6 @@ resource "aws_subnet" "sec-tgw-2c" {
   vpc_id     = aws_vpc.sec-vpc.id
   cidr_block = "10.20.253.0/24"
   availability_zone = "ap-northeast-2c"
-  map_public_ip_on_launch	= true
 
   tags = {
     Name = "sec-tgw-2c"
@@ -78,7 +73,6 @@ resource "aws_subnet" "sec-tgw-2c" {
 
 
 # APPLIANCE VPC AND SUBNET
-
 resource "aws_vpc" "appliance-vpc" {
   cidr_block			= "10.21.0.0/16"
   instance_tenancy		= "default"
@@ -93,7 +87,6 @@ resource "aws_subnet" "appliance-pub-2a" {
   vpc_id     = aws_vpc.appliance-vpc.id
   cidr_block = "10.21.11.0/24"
   availability_zone = "ap-northeast-2a"
-  map_public_ip_on_launch	= true
 
   tags = {
     Name = "appliance-pub-2a"
@@ -104,7 +97,6 @@ resource "aws_subnet" "appliance-pub-2c" {
   vpc_id     = aws_vpc.appliance-vpc.id
   cidr_block = "10.21.13.0/24"
   availability_zone = "ap-northeast-2c"
-  map_public_ip_on_launch	= true
 
   tags = {
     Name = "appliance-pub-2c"
@@ -112,7 +104,6 @@ resource "aws_subnet" "appliance-pub-2c" {
 }
 
 # APP01 VPC AND SUBNET
-
 resource "aws_vpc" "app01-vpc" {
   cidr_block			= "10.10.0.0/16"
   instance_tenancy		= "default"
@@ -127,7 +118,6 @@ resource "aws_subnet" "app01-pri-2a" {
   vpc_id     = aws_vpc.app01-vpc.id
   cidr_block = "10.10.21.0/24"
   availability_zone = "ap-northeast-2a"
-  map_public_ip_on_launch	= true
 
   tags = {
     Name = "app01-pri-2a"
@@ -138,7 +128,6 @@ resource "aws_subnet" "app01-pri-2c" {
   vpc_id     = aws_vpc.app01-vpc.id
   cidr_block = "10.10.23.0/24"
   availability_zone = "ap-northeast-2c"
-  map_public_ip_on_launch	= true
 
   tags = {
     Name = "app01-pri-2c"
@@ -149,7 +138,6 @@ resource "aws_subnet" "app01-tgw-2a" {
   vpc_id     = aws_vpc.app01-vpc.id
   cidr_block = "10.10.251.0/24"
   availability_zone = "ap-northeast-2a"
-  map_public_ip_on_launch	= true
 
   tags = {
     Name = "app01-tgw-2a"
@@ -160,11 +148,11 @@ resource "aws_subnet" "app01-tgw-2c" {
   vpc_id     = aws_vpc.app01-vpc.id
   cidr_block = "10.10.253.0/24"
   availability_zone = "ap-northeast-2c"
-  map_public_ip_on_launch	= true
 
   tags = {
     Name = "app01-tgw-2c"
   }
+
 }
 
 # Internet Gateway
@@ -181,5 +169,52 @@ resource "aws_internet_gateway" "appliance-igw" {
 
   tags = {
     Name = "appliance-igw"
+  }
+}
+
+# NAT Gateway
+resource "aws_nat_gateway" "sec-pub-2a-nat" {
+  allocation_id = aws_eip.sec-pub-2a-nat-eip.id
+  subnet_id     = aws_subnet.sec-pub-2a.id
+
+  tags = {
+    Name = "sec-pub-nat-2a-nat"
+  }
+
+  depends_on = [aws_internet_gateway.sec-igw]
+}
+
+resource "aws_eip" "sec-pub-2a-nat-eip" {
+  vpc   = true
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name = "sec-pub-2a-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "sec-pub-2c-nat" {
+  allocation_id = aws_eip.sec-pub-2c-nat-eip.id
+  subnet_id     = aws_subnet.sec-pub-2c.id
+
+  tags = {
+    Name = "sec-pub-nat-2c-nat"
+  }
+
+  depends_on = [aws_internet_gateway.sec-igw]
+}
+
+resource "aws_eip" "sec-pub-2c-nat-eip" {
+  vpc   = true
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name = "sec-pub-2c-nat-eip"
   }
 }
