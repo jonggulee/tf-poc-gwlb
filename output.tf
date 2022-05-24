75629fb04445ff460fda723294c7c927831c184c
Jonggu Lee <jg.jake.lee@gmail.com>
2022-05-24 12:52:28 +0900

    Update output file

--- output.tf
+++ output.tf
@@ -1,17 +1,13 @@
-# output "appliance_bastion_public_ip" {
-#     value = aws_instance.appliance-bastion-pub-2c.public_ip
-# }
-
 output "sec_bastion_public_ip" {
     value = aws_instance.sec-bastion-pub-2c.public_ip
 }
 
-output "Appliance_01_private_ip" {
-    value = aws_instance.appliance-01-pub-2a.private_ip
+output "appliance_01_public_ip" {
+    value = aws_instance.appliance-01-pub-2a.public_ip
 }
 
-output "Appliance_02_private_ip" {
-    value = aws_instance.appliance-02-pub-2c.private_ip
+output "appliance_02_public_ip" {
+    value = aws_instance.appliance-02-pub-2c.public_ip
 }
 
 output "app01_web_01_private_ip" {
@@ -21,4 +17,3 @@ output "app01_web_01_private_ip" {
 output "app01_web_02_private_ip" {
     value = aws_instance.app01-web-02-pri-2c.private_ip
 }
-
